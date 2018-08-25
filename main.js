72c0bf01727119cd43238a1cfdfadb19e1e2a40e
manoskoutselakis <koutselakismanos@gmail.com>
2018-08-25 21:15:01 +0300

    Added Symbol Class and did some Refactoring'

--- main.js
+++ main.js
@@ -3,7 +3,41 @@ let ctx;
 // let symbols = ['+', '-', '/', '*', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let symbols = ['+', 'x', 'y', '-', '/', '*', '0', '1'];
 let symbolsLen = symbols.length;
-let symbolObjectArray = [];
+let symbolArray = [];
+
+Number.prototype.meow = function (in_min, in_max, out_min, out_max)
+{
+    return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+class Symbol
+{
+    constructor(symbolType, fillStyle, posX, posY, posZ)
+    {
+        this.symbolType = symbolType;
+        this.fillStyle = fillStyle;
+        this.posX = posX;
+        this.posY = posY;
+        this.posZ = posZ;
+    }
+
+    move() 
+    {
+        this.posY -= this.posZ.meow(0, 30, 0, 2);
+        if (this.posY <= 0)
+            this.posY = window.innerHeight + 20;
+    }
+
+    drawSymbol()
+    {
+        this.move();
+        ctx.font = `${this.posZ}px Monospace`;
+        ctx.shadowColor = this.fillStyle;
+        ctx.shadowBlur = 20;
+        ctx.fillStyle = this.fillStyle;
+        ctx.fillText(this.symbolType, this.posX, this.posY);
+    }
+}
 
 window.onload = () =>
 {
@@ -15,12 +49,12 @@ window.onload = () =>
     draw();
 }
 
+
 window.addEventListener('resize', (e) =>
 {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    // draw();
-    //FIXME: WHEN IT RESIZES IT DOENST WORK PROPERLY
+    generateRandomSymbols();
 });
 
 function main()
@@ -38,24 +72,16 @@ function draw()
     requestAnimationFrame(draw);
     ctx.fillStyle = '#292F36';
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-    for (let i = 0; i < symbolObjectArray.length; i++)
+    for (let i = 0; i < symbolArray.length; i++)
     {
-        let object = symbolObjectArray[i];
-        ctx.font = `${symbolObjectArray[i].posZ}px Monospace`;
-        if (symbolObjectArray[i].posZ > 20)
-            symbolObjectArray[i].posY -= 0.5;
-        symbolObjectArray[i].posY -= 0.5;
-        ctx.shadowColor = symbolObjectArray[i].fillStyle;
-        ctx.shadowBlur = 20;
-        ctx.fillStyle = symbolObjectArray[i].fillStyle;
-        ctx.fillText(symbolObjectArray[i].symbolType, symbolObjectArray[i].posX, symbolObjectArray[i].posY);
+        symbolArray[i].drawSymbol();
     }
 }
 
-
 function generateRandomSymbols()
 {
-    ctx.font = `30px Monospace`;
+    symbolArray = [];
+    let maxZ = 30;
     for (let i = 0; i < 200; i++)
     {
         if (i % 2 === 0)
@@ -63,24 +89,16 @@ function generateRandomSymbols()
             let [randomSymbolType, randomPositionX, randomPositionY, randomPositionZ] = [symbols[Math.floor(Math.random() * symbolsLen)],
             Math.random() * window.innerWidth,
             Math.random() * window.innerHeight,
-            Math.floor(Math.random() * 30)];
-            ctx.font = `${randomPositionZ}px Monospace`;
-
-            ctx.fillStyle = '#FF6B6B';
-            ctx.fillText(randomSymbolType, randomPositionX, randomPositionY);
-            symbolObjectArray.push({ symbolType: randomSymbolType, fillStyle: '#FF6B6B', posX: randomPositionX, posY: randomPositionY, posZ: randomPositionZ });
+            Math.floor(Math.random() * maxZ)];
+            symbolArray.push(new Symbol(randomSymbolType, '#FF6B6B', randomPositionX, randomPositionY, randomPositionZ));
         }
         else
         {
             let [randomSymbolType, randomPositionX, randomPositionY, randomPositionZ] = [symbols[Math.floor(Math.random() * symbolsLen)],
             Math.random() * window.innerWidth,
             Math.random() * window.innerHeight,
-            Math.floor(Math.random() * 30)];
-            ctx.font = `${randomPositionZ}px Monospace`;
-
-            ctx.fillStyle = '#4ECDC4';
-            ctx.fillText(randomSymbolType, randomPositionX, randomPositionY);
-            symbolObjectArray.push({ symbolType: randomSymbolType, fillStyle: '#4ECDC4', posX: randomPositionX, posY: randomPositionY, posZ: randomPositionZ });
+            Math.floor(Math.random() * maxZ)];
+            symbolArray.push(new Symbol(randomSymbolType, '#4ECDC4', randomPositionX, randomPositionY, randomPositionZ));
         }
     }
 }
