f211cc49822be5f3e90045218b4a4ce7268249f3
manoskoutselakis <koutselakismanos@gmail.com>
2018-08-25 20:24:48 +0300

    Adding more animation

--- main.js
+++ main.js
@@ -36,13 +36,15 @@ function main()
 function draw()
 {
     requestAnimationFrame(draw);
-    ctx.font = '30px Monospace';
     ctx.fillStyle = '#292F36';
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
     for (let i = 0; i < symbolObjectArray.length; i++)
     {
-        // symbolObjectArray[i].posX += 0.5;
-        // symbolObjectArray[i].posY += 0.5;
+        let object = symbolObjectArray[i];
+        ctx.font = `${symbolObjectArray[i].posZ}px Monospace`;
+        if (symbolObjectArray[i].posZ > 20)
+            symbolObjectArray[i].posY -= 0.5;
+        symbolObjectArray[i].posY -= 0.5;
         ctx.shadowColor = symbolObjectArray[i].fillStyle;
         ctx.shadowBlur = 20;
         ctx.fillStyle = symbolObjectArray[i].fillStyle;
@@ -53,28 +55,32 @@ function draw()
 
 function generateRandomSymbols()
 {
-    ctx.font = '30px Monospace';
-    for (let i = 0; i < 100; i++)
+    ctx.font = `30px Monospace`;
+    for (let i = 0; i < 200; i++)
     {
         if (i % 2 === 0)
         {
-            let [randomSymbolType, randomPositionX, randomPositionY] = [symbols[Math.floor(Math.random() * symbolsLen)],
+            let [randomSymbolType, randomPositionX, randomPositionY, randomPositionZ] = [symbols[Math.floor(Math.random() * symbolsLen)],
             Math.random() * window.innerWidth,
-            Math.random() * window.innerHeight];
+            Math.random() * window.innerHeight,
+            Math.floor(Math.random() * 30)];
+            ctx.font = `${randomPositionZ}px Monospace`;
 
             ctx.fillStyle = '#FF6B6B';
             ctx.fillText(randomSymbolType, randomPositionX, randomPositionY);
-            symbolObjectArray.push({ symbolType: randomSymbolType, fillStyle: '#FF6B6B', posX: randomPositionX, posY: randomPositionY });
+            symbolObjectArray.push({ symbolType: randomSymbolType, fillStyle: '#FF6B6B', posX: randomPositionX, posY: randomPositionY, posZ: randomPositionZ });
         }
         else
         {
-            let [randomSymbolType, randomPositionX, randomPositionY] = [symbols[Math.floor(Math.random() * symbolsLen)],
+            let [randomSymbolType, randomPositionX, randomPositionY, randomPositionZ] = [symbols[Math.floor(Math.random() * symbolsLen)],
             Math.random() * window.innerWidth,
-            Math.random() * window.innerHeight];
+            Math.random() * window.innerHeight,
+            Math.floor(Math.random() * 30)];
+            ctx.font = `${randomPositionZ}px Monospace`;
 
             ctx.fillStyle = '#4ECDC4';
             ctx.fillText(randomSymbolType, randomPositionX, randomPositionY);
-            symbolObjectArray.push({ symbolType: randomSymbolType, fillStyle: '#4ECDC4', posX: randomPositionX, posY: randomPositionY });
+            symbolObjectArray.push({ symbolType: randomSymbolType, fillStyle: '#4ECDC4', posX: randomPositionX, posY: randomPositionY, posZ: randomPositionZ });
         }
     }
 }
